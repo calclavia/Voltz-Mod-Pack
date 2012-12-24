c58eff77238d47c1304e39f401715c5b7b0112b4
cammygames <multicraftserver@ymail.com>
2012-12-24 20:04:45 +0000

    Full Update for 1.4.6

---
+++ Build Pack.bat
@@ -0,0 +1,34 @@
+@echo off
+echo Promotion Type?
+
+set /p CurrentBuild=<buildnumber.txt
+set /a BUILD_NUMBER=%CurrentBuild%+1
+echo %BUILD_NUMBER% >buildnumber.txt
+
+set FILE_NAME=UniversalElectricity_FTB_ModPack_%BUILD_NUMBER%.zip
+
+set FILE_NAME_S=UniversalElectricity_FTB_ModPack_Server_%BUILD_NUMBER%.zip
+
+echo Starting to build the mod pack Client
+
+::ZIP-UP
+echo Starting to Zip up Client
+
+cd Client\
+"7za.exe" a "..\builds\%FILE_NAME%" "*"
+cd ..\
+
+echo Done building %FILE_NAME%
+
+echo Starting to build the mod pack Server
+
+::ZIP-UP
+echo Starting to Zip up Server
+
+cd Server\
+"7za.exe" a "..\builds\%FILE_NAME_S%" "*"
+cd ..\
+
+echo Done building %FILE_NAME_S%
+
+pause
