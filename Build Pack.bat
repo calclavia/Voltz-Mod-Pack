d0b4b892e54441093582f26315f74d9396e64296
cammygames <multicraftserver@ymail.com>
2012-12-31 09:35:01 +0000

    sync my branch

--- Build Pack.bat
+++ Build Pack.bat
@@ -1,11 +1,21 @@
 @echo off
 echo Promotion Type?
 
+set /p CurrentBuildr=<buildnumberr.txt
+set /a BUILD_NUMBER_R=%CurrentBuildr%+1
+echo %BUILD_NUMBER_R% >buildnumberr.txt
+
 set /p CurrentBuild=<buildnumber.txt
-set /a BUILD_NUMBER=%CurrentBuild%+1
+set /a BUILD_NUMBER=%CurrentBuild%
 echo %BUILD_NUMBER% >buildnumber.txt
 
-set FILE_NAME=UniversalElectricity_FTB_ModPack_%BUILD_NUMBER%.zip
+
+set /p CurrentBuildMi=<buildnumberMi.txt
+set /a BUILD_NUMBER_MI=%CurrentBuildMi%
+echo %BUILD_NUMBER_MI% >buildnumber.txt
+
+
+set FILE_NAME=UniversalElectricity_FTB_ModPack_%BUILD_NUMBER%.%BUILD_NUMBER_MI%._%BUILD_NUMBER_MI%.zip
 
 set FILE_NAME_S=UniversalElectricity_FTB_ModPack_Server_%BUILD_NUMBER%.zip
 
